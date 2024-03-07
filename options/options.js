8dacddf4a69de545e40aa4f151d6cc01d484ed20
Crowdbotics <team@crowdbotics.com>
2024-03-07 15:15:27 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mute-mode-46854",
+  url: "https://mute-mode-46854.botics.co",
+  api: "https://mute-mode-46854.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
